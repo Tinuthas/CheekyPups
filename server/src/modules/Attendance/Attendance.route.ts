2c9c152dac1a9a9606717bec30d7b8d2c76ab6c4
Tinuthas <vinicius1504_medeiros@hotmail.com>
2023-03-11 16:47:21 +0000

    update and delete route attendance

--- server/src/modules/Attendance/Attendance.route.ts
+++ server/src/modules/Attendance/Attendance.route.ts
@@ -1,7 +1,7 @@
 import dayjs from "dayjs";
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { prisma } from "../../lib/prisma";
-import { $ref, AttendanceFilterInput, AttendanceInput, DogsAttendanceResult } from "./Attendance.schema";
+import { $ref, AttendanceFilterInput, AttendanceInput, AttendanceUpdateInput, AttendanceUpdatePayInput, DogsAttendanceResult } from "./Attendance.schema";
 
 export async function attendanceRoutes(app: FastifyInstance) {
 
@@ -18,6 +18,30 @@ export async function attendanceRoutes(app: FastifyInstance) {
     },
     preHandler: [app.authenticate]
   }, getAttendancesByDate)
+
+  app.put('/', {
+    schema: {
+      body: $ref('updateAttendanceBody'),
+      querystring: $ref('updateAttendanceId'),
+    },
+    preHandler: [app.authenticate]
+  }, updateAttendanceHandle)
+
+  app.put('/pay', {
+    schema: {
+      body: $ref('updateAttendancePay'),
+      querystring: $ref('updateAttendanceId'),
+    },
+    preHandler: [app.authenticate]
+  }, updateAttendancePayHandle)
+
+  app.delete('/', {
+    schema: {
+      params: {
+        id: { type: 'number' }, // converts the id param to number
+      },
+    }
+  }, deleteAttendanceHandle)   
 
   async function addAttendanceHandle(request: FastifyRequest<{Body: AttendanceInput}>, reply: FastifyReply) {
     try{
@@ -97,6 +121,30 @@ export async function attendanceRoutes(app: FastifyInstance) {
       }
     )
 
+    if(paid == true) {
+      let updAtt = await prisma.attendance.update({
+        where: {
+          id: attendance.id
+        },
+        data: {
+          extract: {
+            create: {
+              value: -value,
+              description: descriptionValue,
+              date: dayjs().toISOString(),
+              Owner: {
+                connect: {
+                  id: dog?.ownerId
+                }
+              }
+            }
+          }
+        }
+      })
+    }
+
+
+
     return attendance
   }
 
@@ -147,9 +195,7 @@ async function getAttendances(input: AttendanceFilterInput){
     ]
   })
 
- 
   let dogsAttendance = new Map<string, { id: number, attendanceIds:[id: number], dog_id:number, name: string, dates:[date:Date], fullDates:[fullDate:boolean], paids:[paid:boolean] }>();
-  console.log('teste')
   for (let index = 0; index < attendances.length; index++) {
     const element = attendances[index];
     console.log(element.dog.id)
@@ -177,4 +223,200 @@ async function getAttendances(input: AttendanceFilterInput){
 
 
   return convertList
+}
+
+async function deleteAttendanceHandle(request: FastifyRequest<{Querystring: {id:number}}>, reply: FastifyReply) {
+  try{
+    return await deleteAttendance(request.query.id)
+  }catch(err) {
+    console.log(err)
+    reply.code(400).send('Error in delete attendance')
+  }
+}
+
+async function deleteAttendance(id: number) {
+
+  let att = await prisma.attendance.findUnique({
+    where: {
+      id: Number(id)
+    }
+  })
+  if(att?.paid != true) {
+    const deleteAttendance = await prisma.attendance.delete({
+      where: {
+        id: Number(id)
+      },
+    })
+    return deleteAttendance
+  }else {
+    return new Error('Attendance cannot be deleted')
+  }
+}
+
+async function updateAttendanceHandle(request: FastifyRequest<{Body: AttendanceUpdateInput, Querystring: {id:number}}>, reply: FastifyReply) {
+  try{
+    return await updateAttendance(request.body, request.query.id)
+  }catch(err) {
+    console.log(err)
+    reply.code(400).send('Error in update attendance')
+  }
+}
+
+
+async function updateAttendance(input: AttendanceUpdateInput, id: number) {
+  const {date, fullDay, paid, value, descriptionValue} = input
+
+  var dateParts:any[] = date.split('/')
+  var dateObject = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0])
+  var parsedDate = dayjs(dateObject).startOf('day').toISOString()
+
+  let att = await prisma.attendance.findUnique({
+    where: {
+      id: Number(id)
+    }, 
+    select: {
+      paid:true,
+      dog: {
+        select: {
+          ownerId: true,
+        },
+      }
+    }
+  })
+
+  if(att?.paid != paid) {
+    let attendanceUpt = await prisma.attendance.update({
+      where: {
+        id: Number(id)
+      },
+      data: {
+        day: {
+          connectOrCreate: {
+            where: {
+              date: parsedDate,
+            },
+            create: {
+              date: parsedDate,
+            }
+          }
+        },
+        fullDay, 
+        paid, 
+        extract : {
+          update: {
+            value,
+            description: descriptionValue,
+            date: dayjs().toISOString(),
+          }
+        }
+      }
+    })  
+  } else {
+    let attendanceUpt = await prisma.attendance.update({
+      where: {
+        id: Number(id)
+      },
+      data: {
+        day: {
+          connectOrCreate: {
+            where: {
+              date: parsedDate,
+            },
+            create: {
+              date: parsedDate,
+            }
+          }
+        },
+        fullDay, 
+        paid, 
+        extract : {
+          create: {
+            value,
+            description: descriptionValue,
+            date: dayjs().toISOString(),
+            Owner: {
+              connect: {
+                id: att?.dog?.ownerId
+              }
+            }
+          }
+        }
+      }
+    })
+  
+  }
+
+  
+  return att
+}
+
+async function updateAttendancePayHandle(request: FastifyRequest<{Body: AttendanceUpdatePayInput, Querystring: {id:number}}>, reply: FastifyReply) {
+  try{
+    return await updatePayAttendance(request.body, request.query.id)
+  }catch(err) {
+    console.log(err)
+    reply.code(400).send('Error in update attendance payment')
+  }
+}
+
+async function updatePayAttendance(input: AttendanceUpdatePayInput, id: number) {
+
+  const {descriptionValue} = input
+
+  let att = await prisma.attendance.findUnique({
+    where: {
+      id: Number(id)
+    }, 
+    select: {
+      paid: true,
+      extract: {
+        select: {
+          id: true,
+          value: true,
+        }
+      },
+      dog: {
+        select: {
+          ownerId: true,
+        },
+      }
+    }
+  })
+  if(att?.paid != true) {
+    let updAtt = await prisma.attendance.update({
+      where: {
+        id: Number(id)
+      },
+      data: {
+        paid: !att?.paid,
+        extract: {
+          create: {
+            value: - att!.extract!.value,
+            description: descriptionValue,
+            date: dayjs().toISOString(),
+            Owner: {
+              connect: {
+                id: att?.dog?.ownerId
+              }
+            }
+          }
+        }
+      }
+    })
+  }else {
+    let updAtt = await prisma.attendance.update({
+      where: {
+        id: Number(id)
+      },
+      data: {
+        paid: !att?.paid,
+      }
+    })
+
+    let extract = await prisma.extract.delete({
+      where: {
+        id: att.extract?.id
+      }
+    })
+  }
 }
--- server/src/modules/Attendance/Attendance.schema.ts
+++ server/src/modules/Attendance/Attendance.schema.ts
@@ -21,13 +21,32 @@ const dogsAttendance = z.object({
   date: z.array(z.coerce.date())
 })
 
+const updateAttendanceBody = z.object({
+  date: z.string(),
+  fullDay: z.boolean(),
+  paid: z.boolean(),
+  value: z.number(),
+  descriptionValue: z.string()
+})
+
+const updateAttendancePay = z.object({
+  descriptionValue: z.string()
+})
+
+const updateAttendanceId = z.object({
+  id: z.number(),
+})
+
 export type AttendanceInput = z.infer<typeof createAttendanceBody>
-
 export type AttendanceFilterInput = z.infer<typeof filterAttendance>
-
 export type DogsAttendanceResult = z.infer<typeof dogsAttendance>
+export type AttendanceUpdateInput = z.infer<typeof updateAttendanceBody>
+export type AttendanceUpdatePayInput = z.infer<typeof updateAttendancePay>
 
 export const {schemas: attendanceSchemas, $ref} = buildJsonSchemas({
   createAttendanceBody,
-  filterAttendance
+  filterAttendance,
+  updateAttendanceBody,
+  updateAttendanceId,
+  updateAttendancePay
 }, { $id: "AttendanceSchemas" })
